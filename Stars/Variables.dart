0068dcaba7665008a3f3403036a583702f9b0c7f
stars <cqlaiyang@gmail.com>
2019-03-29 16:03:16 +0800

    第一次提交

---
+++ Stars/Variables.dart
@@ -0,0 +1,8 @@
+var name = 'Voyager I';
+var year = 1777;
+var antennaDiameter = 3.7;
+var flybyObjects = ['Jupiter','Saturn','Uranus','Neptune'];
+var image = {
+  'tags':['saturn'],
+  'url':'//path/to/saturn.jpg'
+};
